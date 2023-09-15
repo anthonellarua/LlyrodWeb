160a358a0a01193eaa1e08e9bfbc65c4b24ae448
anthonellarua <anthonella.rua@tecsup.edu.pe>
2023-09-14 20:11:54 -0500

    nav

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -6,32 +6,49 @@ import styles from "../styles/navbar.module.css";
 //img
 import Logo from "../public/assets/logo_completo.png";
 
-const pages=[
-  {name: "Industrias", link: "/industrias"},
-  {name: "Sostenibilidad & ESG", link: "/sostenibilidad"},
-  {name: "Trabaja con nosotros", link: "#"},
-  {name: "Contacto", link: "#"},
+const pages = [
+  { name: "Industrias", link: "/industrias" },
+  { name: "Sostenibilidad & ESG", link: "/sostenibilidad" },
+  { name: "Trabaja con nosotros", link: "#" },
+  { name: "Contacto", link: "#" },
 ];
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isFixed, setIsFixed] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <section className={styles.navSection}>
+      <section className={`${styles.navSection} ${isFixed ? styles.fixedNavbar : ""}`}>
         <nav className={styles.navContainer}>
           <Link className={styles.logoContainer} href="/">
-            <Image src={Logo} className={styles.logo} alt="Logo"/>
+            <Image src={Logo} className={styles.logo} alt="Logo" />
           </Link>
           <button className={styles.menuToggle} onClick={toggleMenu}>
             ☰ {/* Icono de hamburguesa */}
           </button>
           <ul className={`${styles.menu} ${menuOpen ? styles.active : ""}`}>
-            {pages.map((page) =>{
+            {pages.map((page) => {
               return (
                 <li className={styles.li} key={page.name}>
                   <Link className={styles.a} href={page.link}>
